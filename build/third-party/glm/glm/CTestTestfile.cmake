0c8abfc516ec9b08322267a22fcebe2afe4a837e
bunnycide <jaebhadane95@gmail.com>
2023-04-18 10:41:02 +0100

    Setup draw calls

--- build/third-party/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jae/CLionProjects/vk-framework/third-party/glm/glm
-# Build directory: /home/jae/CLionProjects/vk-framework/build/third-party/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
